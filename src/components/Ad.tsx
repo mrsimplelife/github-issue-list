ff2f7bc14b2d151b82f31f466d7f0bdce2c344c6
Youme <mrsimplelife@naver.com>
2023-08-31 11:39:17 +0900

    🎨 fix : design 수정

--- src/components/Ad.tsx
+++ src/components/Ad.tsx
@@ -2,11 +2,17 @@ import { keyframes, styled } from 'styled-components';
 
 const Ad = () => {
   return (
-    <a href='https://www.wanted.co.kr/' target='_blank' rel='noopener noreferrer'>
+    <AdLink href='https://www.wanted.co.kr/' target='_blank' rel='noopener noreferrer'>
       <AdImage src='https://image.wanted.co.kr/optimize?src=https%3A%2F%2Fstatic.wanted.co.kr%2Fimages%2Fuserweb%2Flogo_wanted_black.png&w=110&q=100' />
-    </a>
+    </AdLink>
   );
 };
+
+const AdLink = styled.a`
+  display: flex;
+  justify-content: center;
+  padding: 1rem;
+`;
 
 const fadeInScale = keyframes`
     from {
@@ -20,7 +26,11 @@ const fadeInScale = keyframes`
   `;
 
 const AdImage = styled.img`
+  max-width: 300px;
+  max-height: 200px;
   width: 100%;
+  height: auto;
+  object-fit: contain;
   cursor: pointer;
   animation: ${fadeInScale} 0.5s ease-out;
   box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
